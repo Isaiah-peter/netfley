05fee1c5d39e20b45c80d9a3ca1a14497dcd2352
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-08-04 21:01:29 +0100

    discription with hover effect

--- src/component/list/list.jsx
+++ src/component/list/list.jsx
@@ -36,16 +36,16 @@ const List = () => {
           onClick={() => handleClick("left")}
         />}
         <div className="container" ref={listRef}>
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
-          <ListItem />
+          <ListItem index={0}/>
+          <ListItem index={1}/>
+          <ListItem index={2}/>
+          <ListItem index={3}/>
+          <ListItem index={4}/>
+          <ListItem index={5}/>
+          <ListItem index={6}/>
+          <ListItem index={7}/>
+          <ListItem index={8}/>
+          <ListItem index={9}/>
         </div>
         <BiRightArrow
           className="sliderArrow right"
--- src/component/listItem/ListItem.jsx
+++ src/component/listItem/ListItem.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Movies_image from '../../Svg/images.jpg'
+import { MdAdd, MdPlayArrow, MdThumbDown, MdThumbsUpDown, MdThumbUp, } from 'react-icons/md'
 
-function ListItem() {
-    return (
-        <div className='listitem'>
-            welcome
+function ListItem({index}) {
+  const [isHovered, setIsHovered] = useState(false)
+  const trailer = "https://www.youtube.com/embed/MysGjRS9jFU"
+  return (
+    <div className='listitem'
+      style={{left: isHovered && (index * 225) - 50 + (index * 2.5)}}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src={Movies_image} alt="movies" />
+      {isHovered &&(
+      <>
+      <video src={trailer} autoPlay={true} loop />
+      <div className="itemsinfo">
+        <div className="icons">
+          <MdPlayArrow className="icon"/>
+          <MdAdd className="icon"/>
+          <MdThumbUp className="icon"/>
+          <MdThumbDown className="icon"/>
         </div>
-    )
+        <div className="iteminfotop">
+          <span>1 hour and 16 min</span>
+          <span className="limit">+16</span>
+          <span>1999</span>
+        </div>
+        <div className="desc">
+          Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Maiores quisquam omnis
+          a? Esse, ullam dolorem? Molestiae id .
+        </div>
+        <div className="genre">Action thrillers</div>
+      </div>
+      </>
+      )}
+    </div>
+  )
 }
 
 export default ListItem
