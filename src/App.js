a4617ae189290ce8111b484b28f4a98e8f295b90
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-08-07 22:57:25 +0100

    login in page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import Register from "./register/Register";
 import Home from './pages/home/home'
+import Login from "./login/Login";
+import Watch from "./pages/watch/Watch"
+
 
 
 function App() {
   return (
     <div>
-      <Home />
+      <Login />
     </div>
   );
 }
